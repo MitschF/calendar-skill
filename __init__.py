40e6bd1de00dc922dbfffcf0669688a057a31087
Mitsch Fliedner <mail@der-mitsch.de>
2021-01-07 16:22:10 +0100

    Get next appointment

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from mycroft import MycroftSkill, intent_file_handler
 import caldav
+from icalendar import Calendar, Event
 
 from pathlib import Path
 
@@ -20,10 +21,17 @@ class Calendar(MycroftSkill):
       # self.url = "https://" + self.USERNAME + ":" + self.PASSWORD + "@next.social-robot.info/nc/remote.php/dav"
       self.client = caldav.DAVClient(url=self.URL, username=self.USERNAME, password=self.PASSWORD)
       self.principal = self.client.principal()
+      self.calendars = self.principal.calendars()
+      self.calendar = self.calendars[0]
+      self.log.info("USING CALENDAR: " + str(self.calendar))
 
     @intent_file_handler('getAppointment.intent')
     def get_next_appointment(self, message):
-      self.speak(str(self.principal.calendars()))
+      events = self.calendar.events()
+      if len(events) != 0:
+        next_event = events[0]
+      else: 
+        self.speak("You have nothing to do.")
         
 
 
