4b91bb18aba6a33704fad0ab6f68dd14d9a9c617
mh306 <mh306@hdm-stuttgart.de>
2021-01-18 17:58:46 +0100

    added new intent handlers for creating events

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from adapt.intent import IntentBuilder
 import caldav
 from datetime import date, datetime
 from icalendar import Calendar as iCal
@@ -24,7 +25,14 @@ class Calendar(MycroftSkill):
       self.cal = self.calendars[0]
       self.log.info("USING CALENDAR: " + str(self.cal))
 
-    @intent_file_handler('getAppointment.intent')
+      # placeholder for creating new events
+      self.new_Event = {
+        "description" : "placeholder description",
+        "start" : datetime.now(),
+        "allday" : True
+        }
+
+    @intent_file_handler("getAppointment.intent")
     def get_next_appointment(self, message):
       events = self.cal.date_search(datetime.now())
 
@@ -49,7 +57,31 @@ class Calendar(MycroftSkill):
           event_date = next_event.dtstart.value.strftime("%d.%m.%Y")
           self.speak("Next Appointment: {event_summary} on {event_date} at {event_time}".format(event_time=event_time, event_date=event_date, event_summary=next_event.summary.value,))
       
-    # TODO: setAppointment
+          # TODO: überprüfen, ob Skill abschmiert wenn Event ohne Titel in Kalender eingetragen wird 
+
+
+    @intent_file_handler("createAppointment.intent")
+    def get_next_appointment(self, message):
+      self.speak("Description?", expect_response=True)
+
+    @intent_handler(IntentBuilder("createIntent").require("description"))
+    def create_appointment(self, message):
+      self.log.info(message.data.get('utterance'))
+      self.new_Event[description] = message.data.get('utterance')
+      self.speak("Staring time?", expect_response=True)
+
+    @intent_handler(IntentBuilder("createIntent").require("start"))
+    def create_appointment(self, message):
+      self.log.info(message.data.get('utterance'))
+      self.new_Event[start] = message.data.get('utterance')
+      self.speak("Allday?", expect_response=True)
+
+    @intent_handler(IntentBuilder("createIntent").require("allday"))
+    def create_appointment(self, message):
+      self.log.info(message.data.get('utterance'))
+      self.new_Event[allday] = message.data.get('utterance')
+      self.speak("done", new_Event)
+
         
 def create_skill():
     return Calendar()
