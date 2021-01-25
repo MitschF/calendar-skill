ab9b40ee58a86ba0874fc9d99f93f2632faefc5b
Mitsch Fliedner <mail@der-mitsch.de>
2021-01-25 10:32:35 +0100

    Get multiple events

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
 import caldav
 from datetime import date, datetime
 from icalendar import Calendar as iCal
@@ -17,6 +17,9 @@ class Calendar(MycroftSkill):
       self.USERNAME = os.getenv("USERNAME")
       self.PASSWORD = os.getenv("PASSWORD")
       self.URL = os.getenv("URL")
+
+      self.register_entity_file('number.entity')
+
       # open connection to calendar
       self.client = caldav.DAVClient(url=self.URL, username=self.USERNAME, password=self.PASSWORD)
       self.principal = self.client.principal()
@@ -24,8 +27,8 @@ class Calendar(MycroftSkill):
       self.cal = self.calendars[0]
       self.log.info("USING CALENDAR: " + str(self.cal))
 
-    @intent_file_handler('getAppointment.intent')
-    def get_next_appointment(self, message):
+    @intent_file_handler('what.is.next.intent')
+    def handle_what_is_next(self, message):
       events = self.cal.date_search(datetime.now())
 
       if len(events) == 0:
@@ -51,7 +54,39 @@ class Calendar(MycroftSkill):
           self.speak("Next Appointment: {event_summary} on {event_date} at {event_time}"
           .format(event_time=event_time, event_date=event_date, event_summary=next_event.summary.value,))
       
-    # TODO: setAppointment
+    @intent_handler('what.are.next.intent')
+    def handle_what_are_next(self, message):
+      number = message.data.get('number')
+      if number is None:
+        number = 3
+      else:
+        number = int(number)
+      
+      events = self.cal.date_search(datetime.now())
+      if len(events) == 0:
+        self.speak("You have nothing to do!")
+      else:
+        event_list = []
+
+        for event in events:
+          e = event.instance.vevent
+          event_list.append(e)
+
+        event_list.sort(key=lambda e: e.dtstart.value.strftime("%Y-%m-%d, %H:%M"))
+        next_events = event_list[:number]
+        self.speak("Your next " + str(len(next_events)) + " events are: ")
+        for e in next_events:
+          if e.dtstart.value.strftime("%H:%M") == "00:00":
+            # This is an "allday" event
+            event_date = e.dtstart.value.strftime("%d.%m.%Y")
+            self.speak("{event_summary} on {event_date}"
+            .format(event_date=event_date, event_summary=e.summary.value,))
+          else:
+            # This is a "normal" event
+            event_time = e.dtstart.value.strftime("%H:%M")
+            event_date = e.dtstart.value.strftime("%d.%m.%Y")
+            self.speak("{event_summary} on {event_date} at {event_time}"
+            .format(event_time=event_time, event_date=event_date, event_summary=e.summary.value,))
         
 def create_skill():
     return Calendar()
