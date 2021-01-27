c38d129990e60a37e7f76b882be1fb63b72dc780
Mitsch Fliedner <mail@der-mitsch.de>
2021-01-27 15:34:25 +0100

    Reformat what.is and what.are, as well as init.py

--- __init__.py
+++ __init__.py
@@ -27,47 +27,42 @@ class Calendar(MycroftSkill):
       self.cal = self.calendars[0]
       self.log.info("USING CALENDAR: " + str(self.cal))
 
+    def output_event(self, event):
+      if event.dtstart.value.strftime("%H:%M") == "00:00":
+        # This is an "allday" event
+        event_date = event.dtstart.value.strftime("%d.%m.%Y")
+        return("{event_summary} on {event_date}"
+        .format(event_date=event_date, event_summary=event.summary.value,))
+      else:
+        # This is a "normal" event
+        event_time = event.dtstart.value.strftime("%H:%M")
+        event_date = event.dtstart.value.strftime("%d.%m.%Y")
+        return("{event_summary} on {event_date} at {event_time}"
+        .format(event_time=event_time, event_date=event_date, event_summary=event.summary.value,))
+
     @intent_file_handler('what.is.next.intent')
     def handle_what_is_next(self, message):
-      events = self.cal.date_search(datetime.now())
-
-      if len(events) == 0:
-        self.speak("You have nothing to do!")
-      else:
+      events = self.cal.date_search(datetime.now())      
+      if len(events) != 0:
         event_list = []
-
         for event in events:
           e = event.instance.vevent
           event_list.append(e)
 
         event_list.sort(key=lambda e: e.dtstart.value.strftime("%Y-%m-%d, %H:%M"))
         next_event = event_list[0]
-        if next_event.dtstart.value.strftime("%H:%M") == "00:00":
-          # This is an "allday" event
-          event_date = next_event.dtstart.value.strftime("%d.%m.%Y")
-          self.speak("Next Appointment: {event_summary} on {event_date}"
-          .format(event_date=event_date, event_summary=next_event.summary.value,))
-        else:
-          # This is a "normal" event
-          event_time = next_event.dtstart.value.strftime("%H:%M")
-          event_date = next_event.dtstart.value.strftime("%d.%m.%Y")
-          self.speak("Next Appointment: {event_summary} on {event_date} at {event_time}"
-          .format(event_time=event_time, event_date=event_date, event_summary=next_event.summary.value,))
+        self.speak("Your next appointment is: " + self.output_event(next_event))
+      else:
+        self.speak("You have nothing to do!")
       
     @intent_handler('what.are.next.intent')
     def handle_what_are_next(self, message):
-      number = message.data.get('number')
-      if number is None:
-        number = 3
-      else:
-        number = int(number)
-      
-      events = self.cal.date_search(datetime.now())
-      if len(events) == 0:
-        self.speak("You have nothing to do!")
-      else:
-        event_list = []
+      number_attr = message.data.get('number')
+      number = 3 if number_attr is None else int(number_attr)
 
+      events = self.cal.date_search(datetime.now())
+      if len(events) != 0:
+        event_list = []
         for event in events:
           e = event.instance.vevent
           event_list.append(e)
@@ -76,17 +71,9 @@ class Calendar(MycroftSkill):
         next_events = event_list[:number]
         self.speak("Your next " + str(len(next_events)) + " events are: ")
         for e in next_events:
-          if e.dtstart.value.strftime("%H:%M") == "00:00":
-            # This is an "allday" event
-            event_date = e.dtstart.value.strftime("%d.%m.%Y")
-            self.speak("{event_summary} on {event_date}"
-            .format(event_date=event_date, event_summary=e.summary.value,))
-          else:
-            # This is a "normal" event
-            event_time = e.dtstart.value.strftime("%H:%M")
-            event_date = e.dtstart.value.strftime("%d.%m.%Y")
-            self.speak("{event_summary} on {event_date} at {event_time}"
-            .format(event_time=event_time, event_date=event_date, event_summary=e.summary.value,))
+          self.speak(self.output_event(e))
+      else:
+        self.speak("You have nothing to do!")
         
 def create_skill():
     return Calendar()
