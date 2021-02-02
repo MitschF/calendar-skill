9f5376fadf8ffde2a60a59da36669f84f8ac9a25
Mitsch Fliedner <mail@der-mitsch.de>
2021-02-02 12:58:11 +0100

    Reformatting

--- __init__.py
+++ __init__.py
@@ -32,39 +32,35 @@ class Calendar(MycroftSkill):
         Function that returns a formatted string
 
         Args:
-            event (vobject): Calendar-Object
+            event (caldav.objects.Event): Calendar-Object
         Returns:
             string - String in the form of "{event_summary} on {event_date}" or 
             "{event_summary} on {event_date} at {event_time}", 
             depending on whether the event is all-day or not.
         """
-        if event.dtstart.value.strftime("%H:%M") == "00:00":
+        e = event.instance.vevent
+        if e.dtstart.value.strftime("%H:%M") == "00:00":
             self.log.info(type(event))
             # This is an "allday" event
-            event_date = event.dtstart.value.strftime("%d. of %B, %Y")
+            event_date = e.dtstart.value.strftime("%d. of %B, %Y")
             return("{event_summary} on {event_date}"
-                   .format(event_date=event_date, event_summary=event.summary.value, ))
+                   .format(event_date=event_date, event_summary=e.summary.value, ))
         else:
             # This is a "normal" event
-            event_time = event.dtstart.value.strftime("%H:%M")
-            event_date = event.dtstart.value.strftime("%d. of %B, %Y")
+            event_time = e.dtstart.value.strftime("%H:%M")
+            event_date = e.dtstart.value.strftime("%d. of %B, %Y")
             return("{event_summary} on {event_date} at {event_time}"
-                   .format(event_time=event_time, event_date=event_date, event_summary=event.summary.value, ))
+                   .format(event_time=event_time, event_date=event_date, event_summary=e.summary.value, ))
 
     @intent_file_handler('what.is.next.intent')
     def handle_what_is_next(self, message):
         events = self.cal.date_search(datetime.now())
+        events.sort(
+            key=lambda e: e.instance.vevent.dtstart.value.strftime("%Y-%m-%d, %H:%M"))
         if len(events) == 0:
             self.speak("You have nothing to do!")
         else:
-            event_list = []
-            for event in events:
-                e = event.instance.vevent
-                event_list.append(e)
-
-            event_list.sort(
-                key=lambda e: e.dtstart.value.strftime("%Y-%m-%d, %H:%M"))
-            next_event = event_list[0]
+            next_event = events[0]
             self.speak("Your next appointment is: " +
                        self.output_event(next_event))
 
@@ -77,15 +73,10 @@ class Calendar(MycroftSkill):
         if len(events) == 0:
             self.speak("You have nothing to do!")
         else:
-            event_list = []
-            for event in events:
-                e = event.instance.vevent
-                event_list.append(e)
-            
-            event_list.sort(
-                key=lambda e: e.dtstart.value.strftime("%Y-%m-%d, %H:%M"))
-            
-            next_events = event_list[:number]
+            events.sort(
+                key=lambda e: e.instance.vevent.dtstart.value.strftime("%Y-%m-%d, %H:%M"))
+
+            next_events = events[:number]
             self.speak("Your next " + str(len(next_events)) + " events are: ")
             for e in next_events:
                 self.speak(self.output_event(e))
@@ -114,27 +105,10 @@ class Calendar(MycroftSkill):
         if len(events) == 0:
             self.speak("You have nothing to do!")
         else:
-            event_list = []
-
-            for event in events:
-                e = event.instance.vevent
-                event_list.append(e)
-                # self.log.info(event)
-
-            event_list.sort(
-                key=lambda e: e.dtstart.value.strftime("%Y-%m-%d, %H:%M"))
-            for next_event in event_list:
-                if next_event.dtstart.value.strftime("%H:%M") == "00:00":
-                    # This is an "allday" event
-                    event_time = next_event.dtstart.value.strftime("%d.%m.%Y")
-                    self.speak("Next Appointment: {event_summary} on {event_time}".format(
-                        event_time=event_time, event_summary=next_event.summary.value, ))
-                else:
-                    # This is a "normal" event
-                    event_time = next_event.dtstart.value.strftime("%H:%M")
-                    event_date = next_event.dtstart.value.strftime("%d.%m.%Y")
-                    self.speak("Next Appointment: {event_summary} on {event_date} at {event_time}".format(
-                        event_time=event_time, event_date=event_date, event_summary=next_event.summary.value, ))
+            events.sort(
+                key=lambda e: e.instance.vevent.dtstart.value.strftime("%Y-%m-%d, %H:%M"))
+            for next_event in events:
+                self.speak(self.output_event(next_event))
 
     @intent_file_handler("create.appointment.intent")
     def create_appointment(self, message):
@@ -212,19 +186,16 @@ class Calendar(MycroftSkill):
     def delete_event(self, message):
         name = message.data.get('name')
         if name:
-            events = self.cal.date_search(datetime.now())
-            event_list = {
-                "data": [],
-                "names": []
-            }
+            events = list(filter(
+                lambda e: name.lower() in e.instance.vevent.summary.value.lower(), self.cal.date_search(datetime.now())))
+            events.sort(
+                key=lambda e: e.instance.vevent.dtstart.value.strftime("%Y-%m-%d, %H:%M"))
+            event_names = []
             if len(events) == 0:
                 self.speak("You have nothing to do!")
             else:
                 for event in events:
-                    e = event.instance.vevent
-                    if name.lower() in e.summary.value.lower():
-                        event_list["data"].append(event)
-                        event_list["names"].append(e.summary.value)
+                    event_names.append(event.instance.vevent.summary.value)
 
                 def confirm_delete(self, i):
                     """
@@ -232,68 +203,71 @@ class Calendar(MycroftSkill):
                     and deletes the event if the answer is yes
 
                     Args:
-                        i (number): Index
+                        i (number): Index of events-List
                     """
                     confirm_delete = self.ask_yesno('do.you.want.to.delete', {
-                                                    'event_name': event_list["names"][i]})
+                                                    'event_name': event_names[i]})
                     if confirm_delete == 'yes':
-                        event_list["data"][i].delete()
+                        events[i].delete()
                         self.speak('Event {name} deleted.'.format(
-                            name=event_list["names"][i]))
+                            name=event_names[i]))
                     else:
                         self.speak('Okay, I won\'t touch a thing')
 
-                if len(event_list["names"]) == 1:
+                if len(events) == 1:
                     confirm_delete(self, 0)
-                elif len(event_list["names"]) > 1:
+                elif len(events) > 1:
                     self.speak('You have {length} events containing "{name}".'.format(
-                        name=name, length=len(event_list["names"])))
+                        name=name, length=len(events)))
                     selection = self.ask_selection(
-                        event_list["names"], 'Which one do you want do delete?')
-                    index = event_list["names"].index(selection)
+                        event_names, 'Which one do you want do delete?')
+                    index = event_names.index(selection)
                     confirm_delete(self, index)
                 else:
                     self.speak('You have no events containing "{name}".'
                                .format(name=name))
 
     @intent_handler('rename.event.intent')
-    def delete_event(self, message):
+    def rename_event(self, message):
         name = message.data.get('name')
         new_name = message.data.get('new_name')
         if name:
-            events = self.cal.date_search(datetime.now())
-            event_list = {
-                "data": [],
-                "names": []
-            }
+            events = list(filter(
+                lambda e: name.lower() in e.instance.vevent.summary.value.lower(), self.cal.date_search(datetime.now())))
+            events.sort(
+                key=lambda e: e.instance.vevent.dtstart.value.strftime("%Y-%m-%d, %H:%M"))
+            event_names = []
             if len(events) == 0:
                 self.speak("You have nothing to do!")
             else:
                 for event in events:
-                    e = event.instance.vevent
-                    if name.lower() in e.summary.value.lower():
-                        event_list["data"].append(event)
-                        event_list["names"].append(e.summary.value)
+                    event_names.append(event.instance.vevent.summary.value)
 
                 def rename_event(self, i):
-                   event = event_list["data"][i]
-                   event.instance.vevent.summary.value = new_name
-                   event.save()
-                   self.speak("Renamed event {name} to {new_name}.".format(name=event_list["names"][i], new_name=new_name))
+                    """
+                    Deletes event at given index
 
-                if len(event_list["names"]) == 1:
+                    Args:
+                        i (number): Index of events-List
+                    """
+                    event = events[i]
+                    event.instance.vevent.summary.value = new_name
+                    event.save()
+                    self.speak("Renamed event {name} to {new_name}.".format(
+                        name=event_names[i], new_name=new_name))
+
+                if len(events) == 1:
                     rename_event(self, 0)
-                elif len(event_list["names"]) > 1:
+                elif len(events) > 1:
                     self.speak('You have {length} events containing "{name}".'.format(
-                        name=name, length=len(event_list["names"])))
+                        name=name, length=len(events)))
                     selection = self.ask_selection(
-                        event_list["names"], 'Which one do you want do rename?')
-                    index = event_list["names"].index(selection)
+                        event_names, 'Which one do you want do rename?')
+                    index = event_names.index(selection)
                     rename_event(self, index)
                 else:
                     self.speak('You have no events containing "{name}".'
                                .format(name=name))
-
 
 
 def create_skill():
